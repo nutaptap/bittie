8bba0b2e9ec9ec386080be87a6915a2a1f4e2fa8
nutaptap <nutaptap@gmail.com>
2024-03-26 12:06:51 +0100

    Added the login, signup and user buttons to header

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -4,13 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   standalone: true,
   imports: [],
-  template: `
-  <header>
-  <p>Header</p>
-  </header>
-  `,
-  styleUrl: './header.component.css'
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  isLoggedIn = false;
 }
