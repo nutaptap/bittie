440124fac4d7e39d78c6be49fbb3d2e91996689f
nutaptap <nutaptap@gmail.com>
2024-03-28 22:23:31 +0100

    Added cors fix

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { router } from "./routes";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
