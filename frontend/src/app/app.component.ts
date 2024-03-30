6a16a6f01d7824adc4577b8cc136d07de56302bc
nutaptap <nutaptap@gmail.com>
2024-03-30 21:42:33 +0100

    Added form validations

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -30,7 +30,6 @@ export class AppComponent implements OnInit {
 
   constructor(private location: Location) {
     this.currentUrl = this.location.path().substring(1);
-    console.log(this.currentUrl);
   }
 
   fetchData(): Observable<any> {
@@ -38,46 +37,33 @@ export class AppComponent implements OnInit {
   }
 
   findCustom() {
-    console.log('Searching for matching custom url');
-
     this.data.forEach((item) => {
       if (item.custom_url === this.currentUrl) {
-        console.log('Custom url match found: ', item.destination_url);
         this.customUrl = item.destination_url;
       }
     });
-
-    console.log('Redirecting if found');
   }
 
   findId() {
-    console.log('Searching for matching id url');
-
     this.data.forEach((item) => {
       if (String(item.id) === this.currentUrl) {
-        console.log('Id url match found, ', item.destination_url);
         this.customUrl = item.destination_url;
       }
     });
-
-    console.log('Redirecting if found');
   }
 
   redirect() {
-    console.log('Redirecting user');
     if (this.customUrl) {
       window.location.href = this.customUrl;
     } else if (this.idUrl) {
       window.location.href = this.idUrl;
     } else {
-      console.log('No matching url found');
       this.loading = false;
     }
   }
 
   ngOnInit(): void {
     this.fetchData().subscribe((data: any) => {
-      console.log(data);
       this.data = data;
       this.findCustom();
       this.findId();
@@ -85,71 +71,3 @@ export class AppComponent implements OnInit {
     });
   }
 }
-
-/* 
-export class AppComponent implements OnInit {
-  httpClient = inject(HttpClient);
-  data: any[] = [];
-  currentUrl: string = '';
-  customUrl: string | null = null;
-  idUrl: string | null = null;
-  loading = true;
-
-  constructor(private router: Router) {
-    this.currentUrl = this.router.url.substring(1);
-    console.log('current url: ', this.currentUrl);
-  }
-
-  fetchData(): Observable<any> {
-    return this.httpClient.get('http://localhost:1238/');
-  }
-
-  findCustom() {
-    console.log('Searching for matching custom url');
-
-    this.data.forEach((item) => {
-      if (item.custom_url === this.currentUrl) {
-        console.log('Custom url match found: ', item.destination_url);
-        this.customUrl = item.destination_url;
-      }
-    });
-
-    console.log('Redirecting if found');
-  }
-
-  findId() {
-    console.log('Searching for matching id url');
-
-    this.data.forEach((item) => {
-      if (String(item.id) === this.currentUrl) {
-        console.log('Id url match found, ', item.destination_url);
-        this.customUrl = item.destination_url;
-      }
-    });
-
-    console.log('Redirecting if found');
-  }
-
-  redirect() {
-    console.log('Redirecting user');
-    if (this.customUrl) {
-      window.location.href = this.customUrl;
-    } else if (this.idUrl) {
-      window.location.href = this.idUrl;
-    } else {
-      console.log('No matching url found');
-    }
-  }
-
-  ngOnInit(): void {
-    this.fetchData().subscribe((data: any) => {
-      console.log(data);
-      this.data = data;
-      this.findCustom();
-      this.findId();
-      this.redirect();
-      this.loading = false;
-    });
-  }
-}
- */
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  data: any[] = [];
+  private subscription: Subscription | undefined;
+  formSubmitted = false;
+
+  urlForm = new FormGroup({
+    destination: new FormControl('', Validators.required),
+    custom: new FormControl(''),
+  });
+
+  constructor(private httpClient: HttpClient) {}
+
+  fetchData() {
+    this.subscription = this.httpClient
+      .get<any[]>('http://localhost:1238/')
+      .subscribe({
+        next: (data) => {
+          this.data = data;
+          console.log('Data fetched:', this.data);
+        },
+        error: (error) => {
+          console.error('Error fetching data:', error);
+        },
+      });
+  }
+
+  submitForm() {
+    if (this.urlForm.value.destination !== '') {
+      console.log('Url created', this.urlForm.value.destination);
+      this.fetchData();
+      this.formSubmitted = true;
+      return false;
+    } else {
+      console.log('Missing destination url');
+      this.formSubmitted = true;
+      return false;
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}
