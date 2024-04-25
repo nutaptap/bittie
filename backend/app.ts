75f2694495bbcea29fb8de3bcb4691137cfe3919
nutaptap <nutaptap@gmail.com>
2024-04-25 23:28:33 +0200

    Cors fix

--- backend/app.ts
+++ backend/app.ts
@@ -6,12 +6,18 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use("/", router);
 
 const PORT = process.env.PORT || 1238;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server running http://localhost:`, PORT);
+  console.log("Server running");
 });
