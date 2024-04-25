bb0878a48a2f5812a62a2d8bb285a53cac71541a
nutaptap <nutaptap@gmail.com>
2024-04-25 23:47:52 +0200

    Cors

--- backend/app.ts
+++ backend/app.ts
@@ -9,8 +9,6 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
